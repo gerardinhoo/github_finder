9e9b2abed1dcdf62a62226910ba2471aa4298061
Gerard Eklu <gerard_eklu@homedepot.com>
2019-07-28 16:49:57 -0400

    Added clear button to clear users

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -7,7 +7,9 @@ class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onChange = e => {
@@ -20,6 +22,7 @@ class Search extends Component {
     this.setState({ text: "" });
   };
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -36,6 +39,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
