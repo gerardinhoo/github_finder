177bb77f8a05ea12b2014996c46750695b0e2980
Gerard Eklu <gerard_eklu@homedepot.com>
2019-07-28 18:18:51 -0400

    Finished implementing the alert message

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // Fetch github users
@@ -36,8 +38,16 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
+  };
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 4000);
   };
 
   showClear = () => {};
@@ -47,10 +57,12 @@ class App extends Component {
       <div>
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
