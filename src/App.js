238b2762609933254c46a1583d3586f87705c789
Gerard Eklu <gerard_eklu@homedepot.com>
2019-07-29 17:35:46 -0400

    The query to get the single github user was wron. Needed the quotation mark before the client id

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -45,11 +46,11 @@ class App extends Component {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/search/users/${username}&client_id=${
+      `https://api.github.com/users/${username}?client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
+    this.setState({ user: res.data, loading: false });
   };
 
   // Clear users from state
@@ -90,6 +91,7 @@ class App extends Component {
               />
               <Route exact path="/about" component={About} />
               <Route
+                exact
                 path="/user/:login"
                 render={props => (
                   <User
