25cadebb9e2077fa0af0830510c707a84ac3fe7a
Gerard Eklu <gerard_eklu@homedepot.com>
2019-07-29 21:02:14 -0400

    Finished implementing the repos and the github finder app

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -53,6 +54,18 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get Github Repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=6&sort=created:asc&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -66,7 +79,7 @@ class App extends Component {
 
   showClear = () => {};
   render() {
-    const { loading, user, users } = this.state;
+    const { loading, user, users, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -97,7 +110,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
