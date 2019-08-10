8d5d9ac159bee6d5d0c76a31207f9ed3000933f3
Gerard Eklu <gerard_eklu@homedepot.com>
2019-08-10 15:08:01 -0400

    Implemented reduce creators and actions

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -19,7 +19,20 @@ const GithubState = props => {
   };
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
-  // Search Users
+  // Search Github Users
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({ type: SEARCH_USERS, payload: res.data.items });
+    // setUsers(res.data.items);
+    // setLoading(false);
+  };
 
   // Get User
 
@@ -28,6 +41,7 @@ const GithubState = props => {
   // Clear Users
 
   // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -35,7 +49,8 @@ const GithubState = props => {
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
